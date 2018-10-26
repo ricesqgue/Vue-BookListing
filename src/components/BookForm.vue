e070abfcc0424d9b717f469417968a78f172399e
ricesqgue <ricardoeg66@gmail.com>
2018-10-26 14:55:31 -0500

    Completed the module3

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form v-on:submit.prevent="bookSubmit(bookTitle, bookTitle)" class=""  action="#" method="post">
+  <form v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)" class=""  action="#" method="post">
     <input v-model="bookTitle" type="text" name="title" value="" placeholder="Book Title">
     <input v-model="bookAuthor" type="text" name="author" value="" placeholder="Book Author">
     <button type="submit" name="button">Add Book</button>
